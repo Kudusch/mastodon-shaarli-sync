09f2ebeb32d37a1c6bfeb921a26ee02ea891de98
Kudusch <kudusch@posteo.de>
2023-04-23 12:57:25 +0200

    Add timeout for title lookup and better synchronization

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def make_mastodon_header():
 
 def title_from_url(u):
     try:
-        r = requests.get(u)
+        r = requests.get(u, timeout=5)
         soup = BeautifulSoup(r.text, "html.parser")
         return(soup.find("title").get_text())
     except:
@@ -45,14 +45,27 @@ def urls_from_toot(toot):
     else:
         return [toot["url"]]
 
-def add_link(url, toot):
+def update_link(link):
+    payload = {
+        "url": link["url"],
+        "title": link["title"],
+        "description": link["description"],
+        "tags": link["tags"] + [config.TAG_NAME],
+        "private": False,
+        "created": link["created"],
+        "updated": datetime.now().isoformat()
+    }
+    r = requests.put(f"{shaarli_server}/api/v1/links/{link['id']}", json=payload, headers=make_shaarli_header())
+    return(r)
+
+def add_link(url, toot, link=None):
     payload = {
         "url": url,
         "title": title_from_url(url),
         "description": toot["uri"],
         "tags": [
             "unread",
-            "mastodon-bookmark"
+            config.TAG_NAME
         ],
         "private": False,
         "created": datetime.now().isoformat(),
@@ -62,13 +75,28 @@ def add_link(url, toot):
     if r.status_code == 201:
         print("New url saved!")
         print(f"{r.json()['id']}: {r.json()['title']}")
-        return({"url":url,"shaarli_id":r.json()["id"], "toot_id":toot["id"]})
+        return({"url":url,"shaarli_id":r.json()["id"], "toot_id":toot["id"], "created_at":datetime.now().isoformat()})
+    elif r.status_code == 409:
+        if not config.TAG_NAME in r.json()["tags"]:
+            r = update_link(r.json())
+            if r.status_code == 200:
+                print("Existing url updated!")
+                print(f"{r.json()['id']}: {r.json()['title']}")
+                return({"url":url,"shaarli_id":r.json()["id"], "toot_id":toot["id"], "created_at":datetime.now().isoformat()})
+            else:
+                print(r.staus_code, r.json())
+        else:
+            print("Nothing to do")
+            return({"url":url,"shaarli_id":r.json()["id"], "toot_id":toot["id"], "created_at":datetime.now().isoformat()})
+
 
 def delete_bookmark(toot_id=None, shaarli_id=None):
     if toot_id:
+        print("Unbookmarking toot")
         r = requests.post(f"{mastodon_server}/api/v1/statuses/{toot_id}/unbookmark", headers = make_mastodon_header())
     if shaarli_id:
-        r = requests.delete(f"{shaarli_server}/Shaarli/api/v1/links/{shaarli_id}", headers=make_shaarli_header())
+        print("Deleting Shaarli link")
+        r = requests.delete(f"{shaarli_server}/api/v1/links/{shaarli_id}", headers=make_shaarli_header())
 
 def get_toots():
     toots = []
@@ -81,7 +109,7 @@ def get_toots():
     return toots
 
 def get_links():
-    r = requests.get(f"{shaarli_server}/api/v1/links?searchtags={tag_name}", headers=make_shaarli_header())
+    r = requests.get(f"{shaarli_server}/api/v1/links?searchtags={config.TAG_NAME}", headers=make_shaarli_header())
     if (r.status_code == 200):
         return r.json()
     else:
@@ -91,7 +119,6 @@ def get_links():
 if check_config(config):
     mastodon_server = f"https://{config.MASTODON_SERVER}"
     shaarli_server = f"https://{config.SHAARLI_SERVER}"
-    tag_name = config.TAG_NAME
 else:
     print("config error")
 
@@ -105,9 +132,13 @@ bookmarked_toots = get_toots()
 bookmarked_links = get_links()
 for b in old_state:
     if not b["toot_id"] in [t["id"] for t in bookmarked_toots]:
+        print("Change in Mastodon detected, deleting bookmark")
         delete_bookmark(shaarli_id=b["shaarli_id"])
+        bookmarked_links = get_links()
     elif not b["shaarli_id"] in [t["id"] for t in bookmarked_links]:
+        print("Change in Shaarli detected, unbookmarking toot")
         delete_bookmark(toot_id=b["toot_id"])
+        bookmarked_toots = get_toots()
     else:
         state.append(b)
 
@@ -116,8 +147,10 @@ for t in bookmarked_toots:
         for u in urls_from_toot(t):
             state.append(add_link(u, t))
 
-print(state)
-
 with open("state.json", "w") as f:
     json.dump(state, f)
+
+for b in state:
+    print(f"{b['created_at']}: {b['url']}")
+
 
